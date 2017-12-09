d74f4dbf90fa5708f7e30cded0325b434e535835
NK <n1k3r1rus@gmail.com>
2017-12-09 17:37:57 +0700

    ping in ms

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args) => {
-	console.log(`${bot.user.username}: Pong!`) //Лог
-	message.channel.send("Pong!"); //Ответ
+	console.log(`${bot.user.username}: Pong! ${bot.ping}ms`) //Лог
+	message.channel.send(`Pong! ${bot.ping}ms`); //Ответ
 };
 
 module.exports.help = {
